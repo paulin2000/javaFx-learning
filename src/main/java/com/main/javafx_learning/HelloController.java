cf00d55eaddd56656972918fe30894a10624dca9
unknown <paulinlendi2000@gmail.com>
2023-06-04 10:34:31 +0000

    Radio Button - Slider updating

--- src/main/java/com/main/javafx_learning/HelloController.java
+++ src/main/java/com/main/javafx_learning/HelloController.java
@@ -1,7 +1,7 @@
 package com.main.javafx_learning;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -16,6 +16,30 @@ public class HelloController {
     private AnchorPane img;
 
     @FXML
+    private TextField inputfield;
+
+    @FXML
+    private Text textfield;
+
+    @FXML
+    private ToggleGroup Animals;
+
+    @FXML
+    private Text radiovalue;
+
+    @FXML
+    private RadioButton cat;
+
+    @FXML
+    private RadioButton dog;
+
+    @FXML
+    private Slider sliderHorizontal;
+
+    @FXML
+    private Text slidervalue;
+
+    @FXML
     void printHelloWord(MouseEvent event) {
 
     }
@@ -24,14 +48,24 @@ public class HelloController {
     void showImage(MouseEvent event) {
         image.setVisible(true);
     }
-    @FXML
-    private TextField inputfield;
-
-    @FXML
-    private Text textfield;
 
     @FXML
     void nameChanged(MouseEvent event) {
         textfield.setText(inputfield.getText());
+    }
+
+    @FXML
+    void getRadioValue(MouseEvent event) {
+        if (Animals.getSelectedToggle().equals(cat)){
+            radiovalue.setText("Cat");
+        }else {
+            radiovalue.setText("Dog");
+        }
+//      radiovalue.setText(  Animals.getSelectedToggle());
+    }
+
+    @FXML
+    void updateText(MouseEvent event) {
+        slidervalue.setText(String.valueOf(sliderHorizontal.getValue()));
     }
 }
